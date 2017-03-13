4a79b665e559e043be7da1e9462d10eee2276a62
hyc <heyicheng@longwengui.com>
2017-03-13 11:39:24 +0800

    zf

--- longwengui.com/Controller/Admin/Reward.php
+++ longwengui.com/Controller/Admin/Reward.php
@@ -24,12 +24,57 @@ class Reward
      */
     public function RewardLists()
     {
+        $MemberRewardInfoModule = new MemberRewardInfoModule();
+        $SqlWhere = '';
+        // 搜索条件
+        $PageUrl = '';
+        $keyword = trim($_GET['keyword']);
+        if ($keyword != '') {
+            $SqlWhere .= ' and (RewardNum=\'' . $keyword . '\' or concat(DebtName) like \'%' . $keyword . '%\')';
+            $PageUrl .= '&keyword=' . $keyword;
+        }
+        if ($_GET ['Status']) {
+            $Status = trim($_GET ['Status']);
+            $SqlWhere .=' and `Status` = \'' . $Status . '\'';
+            $PageUrl .='&Status=' . $Status;
+        }
+        // 跳转到该页面
+        if ($_POST['page']) {
+            $page = $_POST['page'];
+            tourl('/index.php?Module=Reward&Action=RewardLists&Page=' . $page . $PageUrl);
+        }
+        // 分页开始
+        $Page = intval($_GET['Page']);
+        $Page = $Page ? $Page : 1;
+        $PageSize = 10;
+        $Rscount = $MemberRewardInfoModule->GetListsNum($SqlWhere);
+        if ($Rscount['Num']) {
+            $Data = array();
+            $Data['RecordCount'] = $Rscount['Num'];
+            $Data['PageSize'] = ($PageSize ? $PageSize : $Data['RecordCount']);
+            $Data['PageCount'] = ceil($Data['RecordCount'] / $PageSize);
+            $Data['Page'] = min($Page, $Data['PageCount']);
+            $Offset = ($Page - 1) * $Data['PageSize'];
+
+            if ($Page > $Data['PageCount'])
+                $page = $Data['PageCount'];
+
+            $Data['Data'] = $MemberRewardInfoModule->GetLists($SqlWhere, $Offset, $Data['PageSize']);
+            MultiPage($Data, $Data['PageCount']);
+        }
+        include template('RewardLists');
+    }
+     /**
+     * @desc  悬赏信息详情
+     */
+    public function RewardDetail()
+    {
           $MemberRewardInfoModule = new MemberRewardInfoModule();
         include template('RewardLists');
     }
 
     /**
-     * @desc  悬赏信息
+     * @desc  悬赏信息删除
      */
     public function Delete()
     {
