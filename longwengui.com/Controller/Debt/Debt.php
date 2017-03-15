a1ef9a584cdb36be43e87cc9ae232b74dccd3cba
王泽锋 <wangzefeng@longwengui.com>
2017-03-15 09:54:23 +0800

    zf

--- longwengui.com/Controller/Debt/Debt.php
+++ longwengui.com/Controller/Debt/Debt.php
@@ -24,7 +24,6 @@ class Debt
         $MemberDebtInfoModule = new MemberDebtInfoModule();
         $NStatus = $MemberDebtInfoModule->NStatus;
         $MemberAreaModule = new MemberAreaModule();
-        $MysqlWhere ='';
         //分页查询开始-------------------------------------------------
         $MysqlWhere ='';
         //关键字
@@ -45,11 +44,6 @@ class Debt
             $Offset = ($Page - 1) * $Data['PageSize'];
             $Data['Data'] = $MemberDebtInfoModule->GetLists($MysqlWhere, $Offset,$Data['PageSize']);
             foreach (  $Data['Data'] as $key=>$value){
-                $DebtInfo = json_decode($value['DebtInfo'],true);
-                $Data['Data'][$key]['Name'] = $DebtInfo[0]['name'];
-                $Data['Data'][$key]['Province'] = $DebtInfo[0]['province'];
-                $Data['Data'][$key]['City'] = $DebtInfo[0]['city'];
-                $Data['Data'][$key]['Area'] = $DebtInfo[0]['area'];
 
             }
             $ClassPage = new Page($Rscount['Num'], $PageSize,3);
@@ -60,6 +54,4 @@ class Debt
         $Description="债权人在隆文贵不良资产处置平台发布单笔或多笔债权信息后，债务信息展现在隆文贵不良资产处置债务催收栏目版块，执业律师或催收公司在此页面可根据地域分布和佣金比例等因素选择接单，进行催收，从而赚取佣金。";
         include template('DebtLists');
     }
-
-
 }
