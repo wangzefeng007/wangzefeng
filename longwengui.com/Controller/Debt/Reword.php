43b1f0f958d3c6670576c1a7e2242e64a918a63c
王泽锋 <wangzefeng@longwengui.com>
2017-03-15 16:13:58 +0800

    zf

--- longwengui.com/Controller/Debt/Reword.php
+++ longwengui.com/Controller/Debt/Reword.php
@@ -19,7 +19,7 @@ class Reword
         $MemberRewardInfoModule = new MemberRewardInfoModule();
         $MemberRewardImageModule = new MemberRewardImageModule();
         $MemberAreaModule = new MemberAreaModule();
-        $AreaList = $MemberAreaModule->GetInfoByWhere(' and R1 =1 order by S1 desc',true);var_dump($AreaList);
+        $AreaList = $MemberAreaModule->GetInfoByWhere(' and R1 =1 order by S1 desc',true);
         $MysqlWhere ='';
         //分页查询开始-------------------------------------------------
         $MysqlWhere ='';
