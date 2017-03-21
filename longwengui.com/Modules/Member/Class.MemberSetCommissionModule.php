7baa78cc928e8c96f16e377235802779b558e355
王泽锋 <wangzefeng@longwengui.com>
2017-03-21 09:18:01 +0800

    zf

---
+++ longwengui.com/Modules/Member/Class.MemberSetCommissionModule.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/3/20
+ * Time: 15:19
+ */
+class MemberSetCommissionModule extends CommonModule{
+
+    public $KeyID = 'SetID';
+    public $TableName = 'member_set_commission';
+
+}
