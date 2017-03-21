96b5932b6459eaaa08727766b415e295a2c5daad
王泽锋 <wangzefeng@longwengui.com>
2017-03-21 20:26:22 +0800

    zf

--- longwengui.com/Controller/Debt/Reword.php
+++ longwengui.com/Controller/Debt/Reword.php
@@ -19,7 +19,7 @@ class Reword
         $MemberRewardInfoModule = new MemberRewardInfoModule();
         $MemberRewardImageModule = new MemberRewardImageModule();
         $MemberAreaModule = new MemberAreaModule();
-        $MysqlWhere ='';
+        $MysqlWhere =' and `Status` >2';
         $AreaList = $MemberAreaModule->GetInfoByWhere(' and R1 =1 order by S1 asc',true);
         $MyUrl = WEB_MAIN_URL.'/reword/';
         $SoUrl = $_GET['SoUrl'];
