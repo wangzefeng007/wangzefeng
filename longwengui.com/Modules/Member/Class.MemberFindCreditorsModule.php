8810f0ba970f761f42b1f7380d787333712c7420
王泽锋 <wangzefeng@longwengui.com>
2017-04-06 11:20:16 +0800

    zf

---
+++ longwengui.com/Modules/Member/Class.MemberFindCreditorsModule.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @desc 寻找处置方债权人信息
+ * Class MemberFindCreditorsModule
+ */
+class MemberFindCreditorsModule extends CommonModule {
+    public $KeyID = 'ID';
+    public $TableName = 'member_find_creditors';
+}
