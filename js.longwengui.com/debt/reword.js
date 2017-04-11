25b5e91ec05674d813f5851b7cdf41cc3240711b
huazai122 <1609768987@qq.com>
2017-04-11 19:46:11 +0800

    temp

--- js.longwengui.com/debt/reword.js
+++ js.longwengui.com/debt/reword.js
@@ -40,5 +40,19 @@ function showImgs(imgs){
 
 //选择其他城市情况
 function otherCitySel(){
-  
+  var sel_id = $('#other_city').attr('data-id');
+  var way = $('#way .sel a').html();
+  switch (way) {
+    case '全部':
+      window.location.href = '/reword/' + sel_id + '/'
+      break;
+    case '找人':
+      window.location.href = '/reword/' + sel_id + 'a1/'
+      break;
+    case '找财产':
+      window.location.href = '/reword/' + sel_id + 'a2/' 
+      break;
+    default:
+      return;
+  }
 }
