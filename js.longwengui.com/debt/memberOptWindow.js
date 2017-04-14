744eeb60293793cd8f0f3867f677f9190dae4320
huazai122 <1609768987@qq.com>
2017-04-14 11:41:26 +0800

    temp

--- js.longwengui.com/debt/memberOptWindow.js
+++ js.longwengui.com/debt/memberOptWindow.js
@@ -705,3 +705,42 @@ function debtMatchDetails(details){
       return temp;
     }
 }
+
+//我的悬赏选中图片查看大图
+$('.details-pt-big img').click(function(){
+  var _imgs = [];
+  _imgs.push($(this).attr('src'));
+  $(this).parent().siblings().each(function(){
+    _imgs.push($(this).find('img').attr('src'));
+  });
+  showImgs(_imgs);
+});
+
+$('.details-pt-small img').click(function(){
+  var _imgs = [];
+  _imgs.push($(this).attr('src'));
+  $(this).parent().siblings().each(function(){
+    _imgs.push($(this).find('img').attr('src'));
+  });
+  showImgs(_imgs);
+});
+
+//展示图片
+function showImgs(imgs){
+  layer.open({
+    type: 1,
+    title: 0,
+    area: ["880px", "560px"],
+    closeBtn: 0,
+    shadeClose: true,
+    content:    '<div class="slide-container">'
+              + '</div>'
+  });
+  var _imgData = [
+    {
+      "imgs": imgs
+    }
+  ];
+  $('#slide-tmpl').tmpl(_imgData).appendTo('.slide-container');
+  $('.slide-container').slide({"effect": "fold"});
+}
