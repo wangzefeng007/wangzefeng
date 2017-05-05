11bf3ffbe7d1dcb56356db0232571ded1d496c74
e <islvpan@qq.com>
2017-05-05 14:28:20 +0800

    债权转让发布

--- js.longwengui.com/debt/debtDetails.js
+++ js.longwengui.com/debt/debtDetails.js
@@ -1,33 +1,12 @@
 $(function(){
-
-  $('#debt_imgs').find('img').click(function(){
-    var _imgs = [];
-    _imgs.push($(this).attr('src'));
-    $(this).siblings('img').each(function(){
-      _imgs.push($(this).attr('src'));
+  /*
+  图片预览
+  * */
+    $(".grouped_elements").fancybox({
+        showCloseButton:true,
+        showNavArrows:true
     });
-    showImgs(_imgs);
-  });
 
-  //展示图片
-  function showImgs(imgs){
-    layer.open({
-      type: 1,
-      title: 0,
-      area: ["880px", "560px"],
-      closeBtn: 0,
-      shadeClose: true,
-      content:    '<div class="slide-container">'
-                + '</div>'
-    });
-    var _imgData = [
-      {
-        "imgs": imgs
-      }
-    ];
-    $('#slide-tmpl').tmpl(_imgData).appendTo('.slide-container');
-    $('.slide-container').slide({"effect": "fold"});
-  }
 });
 function ajax(){
   var percent_money = $('#percent_money').val();
--- js.longwengui.com/debt/debtTranPublish.js
+++ js.longwengui.com/debt/debtTranPublish.js
@@ -1,19 +1,22 @@
+//适配ie8
+initIE8Label();
+fixIE8Label();
+
 $(function(){
-  $('#end_time').dcalendarpicker({format: 'yyyy-mm-dd', width: '424px'}); //初始化日期选择器
-  $('#send_time').dcalendarpicker({format: 'yyyy-mm-dd', width: '424px'}); //初始化日期选择器
+  $('#end_time').dcalendarpicker({format: 'yyyy-mm-dd', width: '200px'}); //初始化日期选择器
 });
 
 //截止时间改变时计算距离时间
-function endTimeChange(){
+function endTimeChange(obj){
   var end_time = $('#end_time').val();
-  var day = calcTime(end_time);
+  var day = calcTime(obj,end_time);
   if(day){
     $('#left_time').html('还有<span class="c-b">' + day + '</span>天')
   }
 }
 
 //计算时间距离
-function calcTime(end_time){
+function calcTime(obj,end_time){
   var a = new Date();
   var now = new Date(a.getFullYear() + '-' + ((a.getMonth() + 1) > 9 ? (a.getMonth() + 1) : ('0' + (a.getMonth() + 1))) + '-' + (a.getDate() > 9 ? a.getDate() : ('0' + a.getDate())));
   var end_date = new Date(end_time);
@@ -22,65 +25,237 @@ function calcTime(end_time){
     var dis = end_date.getTime() - now.getTime();
     return dis/(1000*3600*24);
   }else{
+  	showTip(obj, '请选择今天之后的日期');
+  	//$(tar).val('');
     return;
   }
 }
 
 //编辑器初始化
-KindEditor.ready(function(K) {
-	K.each({
-		'plug-align' : {
-			name : '对齐方式',
-			method : {
-				'justifyleft' : '左对齐',
-				'justifycenter' : '居中对齐',
-				'justifyright' : '右对齐'
-			}
-		},
-		'plug-order' : {
-			name : '编号',
-			method : {
-				'insertorderedlist' : '数字编号',
-				'insertunorderedlist' : '项目编号'
-			}
-		},
-    'insertorderedlist' : '数字编号',
-    'insertunorderedlist' : '项目编号',
-		'plug-indent' : {
-			name : '缩进',
-			method : {
-				'indent' : '向右缩进',
-				'outdent' : '向左缩进'
-			}
-		}
-	},function( pluginName, pluginData ){
-		var lang = {};
-		lang[pluginName] = pluginData.name;
-		KindEditor.lang( lang );
-		KindEditor.plugin( pluginName, function(K) {
-			var self = this;
-			self.clickToolbar( pluginName, function() {
-				var menu = self.createMenu({
-						name : pluginName,
-						width : pluginData.width || 100
-					});
-				K.each( pluginData.method, function( i, v ){
-					menu.addItem({
-						title : v,
-						checked : false,
-						iconClass : pluginName+'-'+i,
-						click : function() {
-							self.exec(i).hideMenu();
-						}
-					});
-				})
-			});
-		});
-	});
-	K.create('#contentqq', {
-		themeType : 'qq',
-		items : [
-			'bold', 'italic', 'underline','fontsize','forecolor','plug-align','plug-order','plug-indent','link','image'
-		]
-	});
-});
+
+// 阻止输出log
+// wangEditor.config.printLog = false;
+
+var editor = new wangEditor('editor-trigger');
+
+// 上传图片
+editor.config.uploadImgUrl = '/upload';
+editor.config.uploadParams = {
+    // token1: 'abcde',
+    // token2: '12345'
+};
+editor.config.uploadHeaders = {
+    // 'Accept' : 'text/x-json'
+}
+// editor.config.uploadImgFileName = 'myFileName';
+
+// 隐藏网络图片
+// editor.config.hideLinkImg = true;
+
+// 表情显示项
+editor.config.emotionsShow = 'value';
+editor.config.emotions = {
+    'default': {
+        title: '默认',
+        data: '/Js/wangEditor-2.1.23/emotion.json'
+    },
+    'weibo': {
+        title: '微博表情',
+        data: [
+            {
+                icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/7a/shenshou_thumb.gif',
+                value: '[草泥马]'
+            },
+            {
+                icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/60/horse2_thumb.gif',
+                value: '[神马]'
+            },
+            {
+                icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/bc/fuyun_thumb.gif',
+                value: '[浮云]'
+            },
+            {
+                icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/c9/geili_thumb.gif',
+                value: '[给力]'
+            },
+            {
+                icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/f2/wg_thumb.gif',
+                value: '[围观]'
+            },
+            {
+                icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/70/vw_thumb.gif',
+                value: '[威武]'
+            }
+        ]
+    }
+};
+
+// 插入代码时的默认语言
+// editor.config.codeDefaultLang = 'html'
+
+// 只粘贴纯文本
+// editor.config.pasteText = true;
+
+// 跨域上传
+// editor.config.uploadImgUrl = 'http://localhost:8012/upload';
+
+// 第三方上传
+// editor.config.customUpload = true;
+
+// 普通菜单配置
+editor.config.menus = [
+    'source',
+    '|',
+    'bold',
+    'underline',
+    'italic',
+    'strikethrough',
+    'eraser',
+    'forecolor',
+    'bgcolor',
+    '|',
+    'quote',
+    'fontfamily',
+    'fontsize',
+    'head',
+    'unorderlist',
+    'orderlist',
+    'alignleft',
+    'aligncenter',
+    'alignright',
+    '|',
+    'link',
+    'unlink',
+    'table',
+    'emotion',
+    '|',
+    'img',
+    'video',
+    'location',
+    'insertcode',
+    '|',
+    'undo',
+    'redo',
+    'fullscreen'
+];
+// 只排除某几个菜单（兼容IE低版本，不支持ES5的浏览器），支持ES5的浏览器可直接用 [].map 方法
+// editor.config.menus = $.map(wangEditor.config.menus, function(item, key) {
+//     if (item === 'insertcode') {
+//         return null;
+//     }
+//     if (item === 'fullscreen') {
+//         return null;
+//     }
+//     return item;
+// });
+
+// onchange 事件
+editor.onchange = function () {
+    console.log(this.$txt.html());
+};
+
+// 取消过滤js
+// editor.config.jsFilter = false;
+
+// 取消粘贴过来
+// editor.config.pasteFilter = false;
+
+// 设置 z-index
+// editor.config.zindex = 20000;
+
+// 语言
+// editor.config.lang = wangEditor.langs['en'];
+
+// 自定义菜单UI
+// editor.UI.menus.bold = {
+//     normal: '<button style="font-size:20px; margin-top:5px;">B</button>',
+//     selected: '.selected'
+// };
+// editor.UI.menus.italic = {
+//     normal: '<button style="font-size:20px; margin-top:5px;">I</button>',
+//     selected: '<button style="font-size:20px; margin-top:5px;"><i>I</i></button>'
+// };
+editor.create();
+
+/**表单验证
+ */
+
+function validateForm(){
+    var _trans_money = $("input[name='trans_money']").val();
+    var _sell_phone = $("input[name='sell_phone']").val();
+    var _confirmPass = $("input[name='confirmPass']").val();
+    var _code = $("input[name='code']").val();
+    var agreement = $("input[name='agreement']")[0].checked;
+
+    if(!agreement){
+        showMsg('您还没有同意服务协议');
+        return false;
+    }
+
+    if(_trans_money == '' || _sell_phone == ''){
+        showMsg('请完善注册信息');
+        return false;
+    }
+}
+/*
+* 发布资产转让*/
+$(function(){
+	var $form=$("#myForm");
+	$("#publish").on("click",function(){
+        var formData = validateForm();
+        console.log($form.serialize());
+        if(!formData){
+            return false;
+        }
+		$.ajax({
+			type:"",
+			url:"",
+            dataType: "json",
+			data:{
+                "Intention":"",
+                "AjaxJSON":$form.serialize(),
+			},
+            beforeSend:　function(){
+                showLoading();
+            },success:function(){
+
+			},complete: function(){
+                closeLoading();
+            }
+		})
+	})
+})
+
+//个人证件照片上传裁剪方法
+function imagesInput(tar, ImgBaseData, index) {
+    $.ajax({
+        type: "post",
+        dataType: "json",
+        url: "/loginajax.html",
+        data: {
+            "Intention":"AddCardImage",
+            "ImgBaseData": ImgBaseData,
+        },
+        beforeSend: function () {
+            showLoading();
+        },
+        success: function(data) {
+            if(data.ResultCode=='200'){
+                $(tar).parents(".img-upload-wrap").find(".uploaded-box").append('<div class="img-preview">\
+                    <img src="' + data.url + '" alt="">\
+                    </div>')
+                /*$(tar).parent().siblings('.i-wrap').html(
+                    "<img src='" + data.url + "' alt=''>"
+                );*/
+                setTimeout(function(){
+                    layer.close(index);
+                }, 200);
+            }else{
+                layer.msg(data.Message);
+            }
+        },
+        complete: function () { //加载完成提示
+            closeLoading();
+        }
+    });
+}
