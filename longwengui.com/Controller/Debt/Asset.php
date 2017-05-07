f9b46f0c79e0e8e8bfa97e2d63b14cad66f6dbf2
王泽锋 <wangzefeng@longwengui.com>
2017-05-07 14:29:37 +0800

    zf

--- longwengui.com/Controller/Debt/Asset.php
+++ longwengui.com/Controller/Debt/Asset.php
@@ -11,8 +11,39 @@ class Asset
      * @desc  资产转让列表
      */
     public function Lists(){
+        $MemberAssetInfoModule = new MemberAssetInfoModule();
+        $MemberAssetImageModule = new MemberAssetImageModule();
+        $MemberUserInfoModule = new MemberUserInfoModule();
         $Title ='隆文贵债务处置-资产转让';
         $Nav='index';
+        //分页查询开始-------------------------------------------------
+        $MysqlWhere = ' and `Status` = 2 order by AddTime desc';
+        //关键字
+        $Rscount = $MemberAssetInfoModule->GetListsNum($MysqlWhere);
+        $Page=intval($_GET['p'])?intval($_GET['p']):0;
+        if ($Page < 1) {
+            $Page = 1;
+        }
+        if ($Rscount['Num']) {
+            $PageSize=1;
+            $Data = array();
+            $Data['RecordCount'] = $Rscount['Num'];
+            $Data['PageSize'] = ($PageSize ? $PageSize : $Data['RecordCount']);
+            $Data['PageCount'] = ceil($Data['RecordCount'] / $PageSize);
+            if ($Page > $Data['PageCount'])
+                $Page = $Data['PageCount'];
+            $Data['Page'] = min($Page, $Data['PageCount']);
+            $Offset = ($Page - 1) * $Data['PageSize'];
+            $Data['Data'] = $MemberAssetInfoModule->GetLists($MysqlWhere, $Offset,$Data['PageSize']);
+            foreach ($Data['Data'] as $key=>$value){
+                $AssetImage = $MemberAssetImageModule->GetInfoByWhere(" and AssetID = ".$value['AssetID'].' and IsDefault = 1');
+                $UserInfo = $MemberUserInfoModule->GetInfoByUserID($value['UserID']);
+                $Data['Data'][$key]['ImageUrl'] = $AssetImage['ImageUrl'];
+                $Data['Data'][$key]['RealName'] = $UserInfo['RealName'];
+            }
+            $ClassPage = new Page($Rscount['Num'], $PageSize,3);
+            $ShowPage = $ClassPage->showpage();
+        }
         include template('AssetLists');
     }
     /**
