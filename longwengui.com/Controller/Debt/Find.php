d64ea220a79d10ec807649ed16283e992bbeaee2
王泽锋 <wangzefeng@longwengui.com>
2017-05-23 17:24:01 +0800

    zf

--- longwengui.com/Controller/Debt/Find.php
+++ longwengui.com/Controller/Debt/Find.php
@@ -21,7 +21,55 @@ class Find
     public function ChoiceFind(){
         $Title="寻找处置方-隆文贵债务处置";
         $Nav ='find';
-        include template('DebtChoiceFind');
+        $MemberOrderDemandModule = new MemberOrderDemandModule();
+        $MemberAreaModule = new MemberAreaModule();
+        $MemberUserInfoModule = new MemberUserInfoModule();
+        $MemberUserModule = new MemberUserModule();
+        //分页查询开始-------------------------------------------------
+        $MysqlWhere = ' ';
+        //关键字
+        $Rscount = $MemberOrderDemandModule->GetListsNum($MysqlWhere);
+        $Page=intval($_GET['p'])?intval($_GET['p']):0;
+        if ($Page < 1) {
+            $Page = 1;
+        }
+        if ($Rscount['Num']) {
+            $PageSize=7;
+            $Data = array();
+            $Data['RecordCount'] = $Rscount['Num'];
+            $Data['PageSize'] = ($PageSize ? $PageSize : $Data['RecordCount']);
+            $Data['PageCount'] = ceil($Data['RecordCount'] / $PageSize);
+            if ($Page > $Data['PageCount'])
+                $Page = $Data['PageCount'];
+            $Data['Page'] = min($Page, $Data['PageCount']);
+            $Offset = ($Page - 1) * $Data['PageSize'];
+            $Data['Data'] = $MemberOrderDemandModule->GetLists($MysqlWhere, $Offset,$Data['PageSize']);
+            foreach ($Data['Data'] as $key=>$value){
+                $AreaInfo = json_decode($value['Area'],true);
+                foreach($AreaInfo as $K=>$V){
+                    $AreaInfo[$K]['province'] = $MemberAreaModule->GetCnNameByKeyID($V['province']);
+                    $AreaInfo[$K]['city'] = $MemberAreaModule->GetCnNameByKeyID($V['city']);
+                    $AreaInfo[$K]['area'] = $MemberAreaModule->GetCnNameByKeyID($V['area']);
+                }
+                $Data['Data'][$key]['Area'] = $AreaInfo;
+                $FeeRateInfo = json_decode($value['FeeRate'],true);
+                $Data['Data'][$key]['FeeRate'] = $FeeRateInfo;
+                $User = $MemberUserModule->GetInfoByKeyID($value['UserID']);
+                $UserInfo = $MemberUserInfoModule->GetInfoByUserID($value['UserID']);
+                $Data['Data'][$key]['Mobile'] = $User['Mobile'];
+                $Data['Data'][$key]['Identity'] = $UserInfo['Identity'];
+                $Data['Data'][$key]['Avatar'] = $UserInfo['Avatar'];
+                if ($UserInfo['Identity']==2){
+                    $Data['Data'][$key]['Name'] = $UserInfo['RealName'];
+                }elseif ($UserInfo['Identity']==3){
+                    $Data['Data'][$key]['Name'] = $UserInfo['CompanyName'];
+                }
+            }
+            $ClassPage = new Page($Rscount['Num'], $PageSize,3);
+            $ShowPage = $ClassPage->showpage();
+        }
+     echo "<pre>";print_r($Data['Data']);
+        include template('ChoiceFind');
     }
 
 }
