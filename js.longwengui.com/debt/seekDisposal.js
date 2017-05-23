316769619c5c6aff91b691e50ff14f3d3d451bef
web-oop <islvpan@qq.com>
2017-05-23 18:59:38 +0800

    寻

--- js.longwengui.com/debt/seekDisposal.js
+++ js.longwengui.com/debt/seekDisposal.js
@@ -9,6 +9,41 @@ var pageObj=$.extend({},pageObj,{
     seekShow:function(tar){
         $(tar).parents("li").toggleClass("active");
     },
+    /**
+     * 搜索
+     */
+    search:function(){
+        var dd_province= $(".search-box").find("input[name='dd_province']").siblings("span").attr("data-id");
+        var dd_city= $(".search-box").find("input[name='dd_city']").siblings("span").attr("data-id");
+        var dd_area= $(".search-box").find("input[name='dd_area']").siblings("span").attr("data-id");
+//?dd_province="+dd_province+"&dd_city="+dd_city"
+        $.ajax({
+            type:"get",
+            url:"/loginajax.html",
+            dataType: "json",
+            data:{
+                "Intention":"xxx",
+                "dd_province":dd_province,
+                "dd_city":dd_city,
+                "dd_area":dd_area
+            },
+            beforeSend:　function(){
+                showLoading();
+            },success: function(data){
+                if(data.ResultCode == 200){
+                    showMsg(data.Message);
+                    location.reload();
+                }else{
+                    showMsg(data.Message);
+                }
+            },complete: function(){
+                closeLoading();
+            }
+        })
+    },
+    /**
+     * 投诉建议
+     */
     suggestion:function(){
 
     },
