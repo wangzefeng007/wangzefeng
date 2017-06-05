e6e69f8fcd81d7f8936395966baa097014cf77ee
web-oop <islvpan@qq.com>
2017-06-05 18:06:33 +0800

    手机端

--- m.longwengui.com/js/base.js
+++ m.longwengui.com/js/base.js
@@ -1,16 +1,14 @@
-new function (){
-    var _self = this;
-    var eles,ele;
-    eles = document.documentElement;
-    ele = document.createElement("style");
-    eles.firstElementChild.appendChild(ele);
-    _self.width = 750;//设置默认最大宽度
-    _self.fontSize = 100;//默认字体大小
-    _self.widthProportion = function(){var p = (document.body&&document.body.clientWidth||document.getElementsByTagName("html")[0].offsetWidth)/_self.width;return p>1?1:p<0.426?0.426:p;};
-    _self.changePage = function(){
-        //document.getElementsByTagName("html")[0].setAttribute("style","font-size:"+_self.widthProportion()*_self.fontSize+"px !important");
-        ele.innerHTML="html{font-size:" + _self.widthProportion()*_self.fontSize + "px!important;}";
-    }
-    _self.changePage();
-    window.addEventListener('resize',function(){_self.changePage();},false);
-}
+;(function(doc, win) {//页面rem切图，渲染
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            var p = clientWidth / 750;//页面的最大长度设置为640px
+            p = p > 1 ? 1 : p < 0.5 ? 0.5 : p;
+            docEl.style.fontSize = 100 * p + 'px';//fontSize最大值设置为100px
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
