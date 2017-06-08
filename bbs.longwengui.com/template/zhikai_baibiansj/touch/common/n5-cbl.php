1f3a5640ede5474639aee0f8cb3989b5f44710f2
王泽锋 <wangzefeng@longwengui.com>
2017-06-08 18:11:08 +0800

    zf

--- bbs.longwengui.com/template/zhikai_baibiansj/touch/common/n5-cbl.php
+++ bbs.longwengui.com/template/zhikai_baibiansj/touch/common/n5-cbl.php
@@ -26,14 +26,10 @@
                         </script></a>
 	</div>
 	<div class="n5-cblxm">
-	    <li class="shouye"><a href="forum.php?mod=guide">全站首页</a></li>
-		<li class="luntan"><a href="forum.php?forumlist=1">论坛导航</a></li>
-		<li class="geren"><a href="<!--{if $_G[uid]}-->home.php?mod=space&uid=$_G[uid]&do=profile&mycenter=1<!--{else}-->member.php?mod=logging&action=login<!--{/if}-->">个人中心</a></li>
-		<li class="sousuo"><a href="search.php?mod=forum">搜索一下</a></li>
-		<li class="shoucang"><a href="home.php?mod=space&uid={$_G[uid]}&do=favorite&view=me&type=thread">我的收藏</a></li>
+        <li class="ziliao"><a href="home.php?mod=space&uid={$_G[uid]}">我的资料</a></li>
+        <li class="sousuo"><a href="search.php?mod=forum">搜索一下</a></li>
 		<li class="zhuti"><a href="home.php?mod=space&uid={$_G[uid]}&do=thread&view=me">我的主题</a></li>
-		<li class="xiaoxi"><a href="home.php?mod=space&do=pm">我的消息</a></li>
-		<li class="ziliao"><a href="home.php?mod=space&uid={$_G[uid]}">我的资料</a></li>
+        <li class="shoucang"><a href="home.php?mod=space&uid={$_G[uid]}&do=favorite&view=me&type=thread">我的收藏</a></li>
 	</div>
 	
 </div>				
--- bbs.longwengui.com/uc_server/data/logs/201706.php
+++ bbs.longwengui.com/uc_server/data/logs/201706.php
@@ -3,3 +3,4 @@
 <?PHP exit;?>	admin	127.0.0.1	1496388436	login	succeed
 <?PHP exit;?>	admin	127.0.0.1	1496396803	login	succeed
 <?PHP exit;?>	admin	127.0.0.1	1496403224	login	succeed
+<?PHP exit;?>	admin	127.0.0.1	1496910531	login	succeed
