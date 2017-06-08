783fe7ec92506cd5a7f9bae476a5420507fd9167
王泽锋 <wangzefeng@longwengui.com>
2017-06-08 10:15:51 +0800

    zf

--- longwengui.com/Controller/M/MemberPerson.php
+++ longwengui.com/Controller/M/MemberPerson.php
@@ -47,6 +47,27 @@ class MemberPerson
      * @desc 编辑信息
      */
     public function EditInfo(){
+        MService::IsNoLogin();
+        $MemberUserModule = new MemberUserModule();
+        $MemberUserInfoModule = new MemberUserInfoModule();
+        $MemberAreaModule = new MemberAreaModule();
+        $IdentityStatus = $MemberUserInfoModule->IdentityStatus;
+        $Identity = $MemberUserInfoModule->Identity;
+        //会员基本信息
+        $User = $MemberUserModule->GetInfoByKeyID($_SESSION['UserID']);
+        $UserInfo = $MemberUserInfoModule->GetInfoByUserID($_SESSION['UserID']);
+        if ($UserInfo['Province'])
+            $UserInfo['Province'] = $MemberAreaModule->GetCnNameByKeyID($UserInfo['Province']);
+        if ($UserInfo['City'])
+            $UserInfo['City'] = $MemberAreaModule->GetCnNameByKeyID($UserInfo['City']);
+        if ($UserInfo['Area'])
+            $UserInfo['Area'] = $MemberAreaModule->GetCnNameByKeyID($UserInfo['Area']);
         include template('MemberPersonEditInfo');
+    }
+    /**
+     * @desc 关注/收藏
+     */
+    public function Focus(){
+        include template('MemberPersonFocus');
     }
 }
