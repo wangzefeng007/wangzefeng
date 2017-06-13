244cff8e20908c2f3575fb9271b76a68856b8dae
王泽锋 <wangzefeng@longwengui.com>
2017-06-13 16:41:39 +0800

    zf

--- longwengui.com/Controller/M/Asset.php
+++ longwengui.com/Controller/M/Asset.php
@@ -15,7 +15,6 @@ class Asset
         $MemberAssetImageModule = new MemberAssetImageModule();
         $MemberUserInfoModule = new MemberUserInfoModule();
         include template('AssetIndex');
-        echo 'asset';exit;
     }
     /**
      * @desc  发布资产转让
@@ -59,9 +58,9 @@ class Asset
      */
     public function  Order(){
         MService::IsNoLogin();
-        if ($_SESSION ['Identity']!=1 && $_SESSION['Identity']!=2){
-            alertandback("个人会员和催客方可购买商品！");
-        }
+//        if ($_SESSION ['Identity']!=1 && $_SESSION['Identity']!=2){
+//            alertandback("个人会员和催客方可购买商品！");
+//        }
         include template('AssetOrder');
     }
     /**
@@ -69,12 +68,150 @@ class Asset
      */
     public function ChoicePay()
     {
-
+        $Title = '订单支付';
+        $OrderNumber = $_GET['OrderNumber'];
+        $MemberUserInfoModule = new MemberUserInfoModule();
+        $MemberAssetInfoModule = new MemberAssetInfoModule();
+        $MemberProductOrderModule = new MemberProductOrderModule();
+        $OrderInfo = $MemberProductOrderModule->GetInfoByWhere(' and OrderNumber = \''.$OrderNumber.'\'');
+        $AssetInfo = $MemberAssetInfoModule->GetInfoByKeyID($OrderInfo['ProductID']);
+        $UserInfo = $MemberUserInfoModule->GetInfoByUserID($AssetInfo['UserID']);
+        $OrderInfo['Title'] = $AssetInfo['Title'];
+        $OrderInfo['RealName'] = $UserInfo['RealName'];
+        $GoToUrl = WEB_M_URL . '/member/buyorderlist/';
+        $Time = time();
+        if ($OrderInfo && $OrderInfo['Status'] == 1) {
+            if ($OrderInfo['ExpirationTime'] > $Time) {
+                include template('AssetOrderPay');
+            } else {
+                $UpData['Status'] = 10;
+                $UpData['Remarks'] = '订单超时未支付';
+                $Result = $MemberProductOrderModule->UpdateInfoByKeyID($UpData, $OrderInfo['OrderID']);
+                if ($Result) {
+                    $LogMessage = '操作失败(超时状态更新失败)';
+                } else {
+                    $LogMessage = '超时未支付,订单取消';
+                }
+                // 添加订单状态更改日志
+                $OrderLogModule = new MemberOrderLogModule();
+                if ($_SESSION['UserID'] && ! empty($_SESSION['UserID'])) {
+                    $UserID = $_SESSION['UserID'];
+                }
+                $LogData = array(
+                    'OrderNumber' => $OrderNumber,
+                    'UserID' => $UserID,
+                    'OldStatus' => 1,
+                    'NewStatus' => 10,
+                    'OperateTime' => date("Y-m-d H:i:s", time()),
+                    'IP' => GetIP(),
+                    'Remarks' => $LogMessage,
+                    'Type' => 1
+                );
+                $LogResult = $OrderLogModule->InsertInfo($LogData);
+                alertandgotopage('订单超时未支付', $GoToUrl);
+            }
+        } else {
+            alertandgotopage('不能操作的订单', $GoToUrl);
+        }
     }
+
     /**
      * 准备支付
      */
     public function Pay()
     {
+        $Type = trim($_GET['type']);
+        $OrderID = trim($_GET['id']);
+        $MemberAssetInfoModule = new MemberAssetInfoModule();
+        $MemberProductOrderModule = new MemberProductOrderModule();
+        $Order = $MemberProductOrderModule->GetInfoByKeyID($OrderID);
+        $AssetInfo = $MemberAssetInfoModule->GetInfoByKeyID($Order['ProductID']);
+        $Data = array();
+        if ($_POST){
+            var_dump($_POST);exit;
+        }
+        if ($Order && $Order['Status'] == 1) {
+            if ($Type == 'alipay') {
+                $Data['OrderNo'] = $Order['OrderNumber'];
+                $Data['Subject'] = html_entity_decode($AssetInfo['Title'], ENT_QUOTES);
+                $Data['Money'] = $Order['TotalAmount'];
+                $Data['Body'] = html_entity_decode($AssetInfo['Title'], ENT_QUOTES);
+                $Data['ReturnUrl'] = WEB_M_URL . '/pay/result/';
+                $Data['NotifyUrl'] = WEB_M_URL . '/pay/result/';
+                $Data['ProductUrl'] = WEB_M_URL . "/assetdetails/{$Order['ProductID']}.html";
+                $Data['RunTime'] = time();
+                $Data['Sign'] = ToolService::VerifyData($Data);
+                echo ToolService::PostForm(WEB_M_URL . '/pay/alipay/', $Data);
+            } elseif ($Type == 'wxpay') {
+                $Data['OrderNo'] = $Order['OrderNumber'];
+                $Data['Subject'] = html_entity_decode($AssetInfo['Title'], ENT_QUOTES);
+                $Data['Money'] = $Order['TotalAmount'];
+                $Data['Body'] = html_entity_decode($AssetInfo['Title'], ENT_QUOTES);
+                $Data['ReturnUrl'] = WEB_M_URL . '/pay/result/';
+                $Data['RunTime'] = time();
+                $Data['Sign'] = ToolService::VerifyData($Data);
+                $Sign = $Data['Sign'];
+                unset($Data['Sign']);
+                if ($Sign == ToolService::VerifyData($Data)) {
+                    include SYSTEM_ROOTPATH . '/Include/WXPayTwo/WxPay.NativePay.php';
+                    $notify = new NativePay();
+                    $input = new WxPayUnifiedOrder();
+                    $input->SetBody(stripslashes($Data['Subject'])); //必填
+                    $input->SetDetail(stripslashes($Data['Body']));
+                    $input->SetOut_trade_no($Data['OrderNo']); //必填
+                    $input->SetTotal_fee($Data['Money']*100); //必填
+                    $input->SetNotify_url(WEB_M_URL . '/pay/wxpaynotify/'); //必填
+                    $input->SetTrade_type("NATIVE");
+                    $input->SetSpbill_create_ip(GetIP());
+                    $input->SetProduct_id($Data['OrderNo']);
+                    $result = $notify->GetPayUrl($input);
+                    if ($result['code_img_url'] && $result['status']=='0') {
+                        $ImageUrl = $result["code_img_url"];
+                        $result_json = array('ResultCode'=>200,'Message'=>'返回成功','ImageUrl'=>$ImageUrl);
+                        echo json_encode($result_json,JSON_UNESCAPED_UNICODE);exit;
+                    }elseif ($result['err_msg']=='订单已支付'&& $result['result_code']=='1'){
+                        //更新库存量
+                        $MemberAssetInfoModule = new MemberAssetInfoModule();
+                        $MemberAssetInfoModule->SetInventory($Order['ProductID'],$Order['Num']);
+                        //添加订单日志
+                        $OrderLogModule = new MemberOrderLogModule();
+                        $LogMessage ='买家已付款，付款方式微信支付';
+                        $LogData = array(
+                            'OrderNumber' =>$Data['OrderNo'],
+                            'UserID' => $_SESSION['UserID'],
+                            'OldStatus' => 1,
+                            'NewStatus' => 2,
+                            'OperateTime' => date("Y-m-d H:i:s", time()),
+                            'IP' => GetIP(),
+                            'Remarks' => $LogMessage,
+                            'Type' => 1
+                        );
+                        $LogResult = $OrderLogModule->InsertInfo($LogData);
+                        //添加订单状态
+                        $Date['PaymentMethod'] = '2';
+                        $Date['Status'] = '2';
+                        $MemberProductOrderModule->UpdateInfoByWhere($Date,' OrderNumber = \''.$Data['OrderNo'].'\'');//更新订单状态
+                        $VerifyData['OrderNo'] = $Data['OrderNo'];
+                        $VerifyData['Money'] = ($Data['Money']);
+                        $VerifyData['PayType'] = "微信支付";
+                        $VerifyData['ResultCode'] = 'SUCCESS';
+                        $VerifyData['RunTime'] = time();
+                        $VerifyData['RedirectUrl'] = WEB_M_URL . '/orderdetail/'.$VerifyData['OrderNo'].'.html';
+                        $VerifyData['Sign'] = ToolService::VerifyData($VerifyData);
+                        $result_json = array('ResultCode'=>200,'Message'=>'支付成功','Url'=>WEB_M_URL.'/pay/wxresult/?OrderNo='.$Data['OrderNo']);
+                        EchoResult($result_json);
+                    } else {
+                        $result_json = array('ResultCode'=>102,'Message'=>'订单异常','Url'=>WEB_M_URL);
+                        EchoResult($result_json);
+                    }
+                    //判断支付成功返回支付成功结果页end
+                } else {
+                    $result_json = array('ResultCode'=>103,'Message'=>'异常的请求','Url'=>WEB_M_URL);
+                }
+                EchoResult($result_json);exit;
+            }
+        } else {
+            alertandback('不能操作的订单');
+        }
     }
 }
---
+++ longwengui.com/Controller/M/Pay.php
@@ -0,0 +1,244 @@
+<?php
+
+class Pay
+{
+
+    //支付宝
+    /**
+     * @desc  支付宝
+     * @throws Exception
+     */
+    public function AliPay()
+    {
+        include SYSTEM_ROOTPATH.'/Include/Alipay/wap/AopSdk.php';
+        $aop = new AopClient();
+        $aop->gatewayUrl = 'https://openapi.alipay.com/gateway.do';
+        $aop->appId = '2088912584072026';
+        $aop->rsaPrivateKeyFilePath = SYSTEM_ROOTPATH.'/Include/Alipay/wap/rsa_private_key.pem';
+        $aop->alipayPublicKey=SYSTEM_ROOTPATH.'/Include/Alipay/wap/rsa_public_key.pem';
+        $aop->apiVersion = '1.0';
+        $aop->postCharset='utf-8';
+        $aop->format='json';
+        $request = new AlipayTradeWapPayRequest ();
+        $JsonData['body']='测试订单';
+        $JsonData['subject']='测试订单';
+        $JsonData['out_trade_no']='ceshi3423423';
+        $JsonData['total_amount']='0.01';
+        $JsonData['product_code']="QUICK_WAP_PAY";
+        $request->setReturnUrl(WEB_M_URL . '/pay/wapalipayreturn/');
+        $request->setNotifyUrl(WEB_M_URL . '/pay/wapalipaynotify/');
+        $request->setBizContent(json_encode($JsonData));
+        $result = $aop->pageExecute ( $request);
+        echo $result;exit;
+        $Sign = $_POST['Sign'];
+        unset($_POST['Sign']);
+        if ($Sign == ToolService::VerifyData($_POST)) {
+            $notify_url = WEB_M_URL . '/pay/alipaynotify/';
+            $return_url = WEB_M_URL . '/pay/alipaynotify/'; //必填
+            $out_trade_no = $_POST['OrderNo']; //必填
+            $subject = stripslashes($_POST['Subject']); //必填
+            $total_fee = $_POST['Money']; //必填
+            $body = stripslashes($_POST['Body']);
+            $show_url = $_POST['ProductUrl'];
+            $MemberOrderTempModule = new MemberOrderTempModule();
+            $Data['OrderID'] = $out_trade_no;
+            $Data['NotifyUrl'] = $_POST['ReturnUrl'];
+            $Data['PayType'] = 0;
+            $Data['CreateTime'] = time();
+            $Data['ResultCode'] = 0;
+            $OrderExists = $MemberOrderTempModule->GetOrderByID($Data['OrderID']);
+            if (!$OrderExists) {
+                $InsertResult = $MemberOrderTempModule->InsertInfo($Data);
+            } else {
+                if ($OrderExists['ResultCode'] == 1) {
+                    alertandgotopage("该订单已支付完成!", WEB_M_URL);
+                } else {
+                    $InsertResult = $MemberOrderTempModule->UpdateData($Data, $Data['OrderID']);
+                }
+            }
+            if ($InsertResult) {
+                if($this->IsOrNotMobile()){
+                    include SYSTEM_ROOTPATH.'/Include/Alipay/wap/AopSdk.php';
+                    $aop = new AopClient();
+                    $aop->gatewayUrl = 'https://openapi.alipay.com/gateway.do';
+                    $aop->appId = '2088912584072026';
+                    $aop->rsaPrivateKeyFilePath = SYSTEM_ROOTPATH.'/Include/Alipay/wap/rsa_private_key.pem';
+                    $aop->alipayPublicKey=SYSTEM_ROOTPATH.'/Include/Alipay/wap/rsa_public_key.pem';
+                    $aop->apiVersion = '1.0';
+                    $aop->postCharset='utf-8';
+                    $aop->format='json';
+                    $request = new AlipayTradeWapPayRequest ();
+                    $JsonData['body']=$body;
+                    $JsonData['subject']=$subject;
+                    $JsonData['out_trade_no']=$out_trade_no;
+                    $JsonData['total_amount']=$total_fee;
+                    $JsonData['product_code']="QUICK_WAP_PAY";
+                    $request->setReturnUrl(WEB_M_URL . '/pay/wapalipayreturn/');
+                    $request->setNotifyUrl(WEB_M_URL . '/pay/wapalipaynotify/');
+                    $request->setBizContent(json_encode($JsonData));
+                    $result = $aop->pageExecute ( $request);
+                    echo $result;
+                }else{
+                    include SYSTEM_ROOTPATH.'/Include/AliPay/AliPay.php';
+                    $AliPay = new AliPay();
+                    $AliPay->SubmitOrder(1, $notify_url, $return_url, $out_trade_no, $subject, $total_fee, $body, $show_url);
+                }
+            } else {
+                alertandgotopage('订单支付出现异常,请重新尝试', WEB_M_URL);
+            }
+        } else {
+            alertandgotopage('异常的请求', WEB_M_URL);
+        }
+
+    }
+
+    //支付宝回调
+    public function AliPayNotify()
+    {
+        $MemberProductOrderModule = new MemberProductOrderModule;
+        include SYSTEM_ROOTPATH.'/Include/AliPay/AliPay.php';
+        $AliPay = new AliPay();
+        $ResultUrl = WEB_M_URL . '/pay/result/';
+        if (count($_POST)) {
+            if ($AliPay->GetPayStatus($_POST) === 'true') {
+                $OrderNumber = trim($_POST['out_trade_no']);
+                $OrderInfo = $MemberProductOrderModule ->GetInfoByWhere(' and OrderNumber = \''.$OrderNumber.'\'');
+                if ($OrderInfo) {
+                    //更新库存
+                    $MemberAssetInfoModule = new MemberAssetInfoModule();
+                    $MemberAssetInfoModule->SetInventory($OrderInfo['ProductID'],$OrderInfo['Num']);
+                    //添加订单日志
+                    $OrderLogModule = new MemberOrderLogModule();
+                    $LogMessage ='买家已付款，付款方式支付宝';
+                    $LogData = array(
+                        'OrderNumber' =>$OrderInfo['OrderNumber'],
+                        'UserID' => $_SESSION['UserID'],
+                        'OldStatus' => 1,
+                        'NewStatus' => 2,
+                        'OperateTime' => date("Y-m-d H:i:s", time()),
+                        'IP' => GetIP(),
+                        'Remarks' => $LogMessage,
+                        'Type' => 1
+                    );
+                    $LogResult = $OrderLogModule->InsertInfo($LogData);
+                    //更新订单状态
+                    $Data['PaymentMethod'] = '1';
+                    $Data['Status'] = '2';
+                    $MemberProductOrderModule->UpdateInfoByWhere($Data,' OrderNumber = \''.$OrderNumber.'\'');
+                    $VerifyData['OrderNo'] = trim($_POST['out_trade_no']);
+                    $VerifyData['Money'] = $OrderInfo['TotalAmount'];
+                    $VerifyData['ResultCode'] = 'SUCCESS';
+                    $VerifyData['RunTime'] = time();
+                    $VerifyData['RedirectUrl'] = WEB_M_URL . '/orderdetail/'.$VerifyData['OrderNo'].'.html';
+                    $VerifyData['Sign'] = ToolService::VerifyData($VerifyData);
+                    echo ToolService::PostForm($ResultUrl, $VerifyData);
+                } else {
+                    header("Location:/pay/result/");
+                }
+            } else {
+                header("Location:/pay/result/");
+            }
+        } else {
+            if ($AliPay->GetPayStatus($_GET) === 'true') {
+                $OrderNumber = trim($_GET['out_trade_no']);
+                $OrderInfo = $MemberProductOrderModule ->GetInfoByWhere(' and OrderNumber = \''.$OrderNumber.'\'');
+                if ($OrderInfo) {
+                    //更新库存
+                    $MemberAssetInfoModule = new MemberAssetInfoModule();
+                    $MemberAssetInfoModule->SetInventory($OrderInfo['ProductID'],$OrderInfo['Num']);
+                    //添加订单日志
+                    $OrderLogModule = new MemberOrderLogModule();
+                    $LogMessage ='买家已付款，付款方式支付宝';
+                    $LogData = array(
+                        'OrderNumber' =>$OrderInfo['OrderNumber'],
+                        'UserID' => $_SESSION['UserID'],
+                        'OldStatus' => 1,
+                        'NewStatus' => 2,
+                        'OperateTime' => date("Y-m-d H:i:s", time()),
+                        'IP' => GetIP(),
+                        'Remarks' => $LogMessage,
+                        'Type' => 1
+                    );
+                    $LogResult = $OrderLogModule->InsertInfo($LogData);
+                    //更新订单状态
+                    $Data['PaymentMethod'] = '1';
+                    $Data['Status'] = '2';
+                    $MemberProductOrderModule->UpdateInfoByWhere($Data,' OrderNumber = \''.$OrderNumber.'\'');
+                    $VerifyData['OrderNo'] = trim($_GET['out_trade_no']);
+                    $VerifyData['Money'] = $OrderInfo['TotalAmount'];
+                    $VerifyData['ResultCode'] = 'SUCCESS';
+                    $VerifyData['RunTime'] = time();
+                    $VerifyData['RedirectUrl'] = WEB_M_URL . '/orderdetail/'.$VerifyData['OrderNo'].'.html';
+                    $VerifyData['Sign'] = ToolService::VerifyData($VerifyData);
+                    echo ToolService::PostForm($ResultUrl, $VerifyData);
+                } else {
+                    header("Location:/pay/result/");
+                }
+            } else {
+                header("Location:/pay/result/");
+            }
+        }
+    }
+    /**
+     * @desc 微信支付
+     */
+    public function WXPay()
+    {
+            include SYSTEM_ROOTPATH . '/Include/WXPayTwo/WxPay.NativePay.php';
+            $notify = new NativePay();
+            $input = new WxPayUnifiedOrder();
+            $input->SetBody('测试订单'); //必填
+            $input->SetDetail('测试订单');
+            $input->SetOut_trade_no('db2342342334234'); //必填
+            $input->SetTotal_fee(1*100); //必填
+            $input->SetNotify_url(WEB_M_URL . '/pay/wxpaynotify/'); //必填
+            $input->SetTrade_type("NATIVE");
+            $input->SetSpbill_create_ip(GetIP());
+            $input->SetProduct_id('db2342342334234');
+            $result = $notify->GetPayUrl($input);var_dump($result);exit;
+            if ($result['code_img_url']) {
+//                    $WXPayUrl= $result['code_url'];
+//                    $WXPayUrl = "http://paysdk.weixin.qq.com/example/qrcode.php?data=" . urlencode($WXPayUrl);
+                $ImageUrl = $result["code_img_url"];
+                $result_json = array('ResultCode'=>200,'Message'=>'返回成功','ImageUrl'=>$ImageUrl);
+            } else {
+                $result_json = array('ResultCode'=>102,'Message'=>'订单异常','Url'=>WEB_M_URL);
+                alertandgotopage('订单异常', WEB_M_URL);
+            }
+        EchoResult($result_json);exit;
+    }
+    //支付成功提示
+    public function Result()
+    {
+        $sign = $_POST['Sign'];
+        unset($_POST['Sign']);
+        $VerifySign = ToolService::VerifyData($_POST);
+        if ($VerifySign == $sign) {
+            $PayResult = $_POST['ResultCode'];
+            if ($PayResult == 'SUCCESS') {
+                $OrderNumber = $_POST['OrderNo'];
+                $Money = $_POST['Money'];
+                $RedirectUrl = $_POST['RedirectUrl'];
+                include template('PayResultSUCCESS');
+            } else {
+                include template('PayResultFAIL');
+            }
+        } else {
+            include template('PayResultFAIL');
+        }
+    }
+    //支付成功提示
+    public function WxResult()
+    {
+        $OrderNumber = $_GET['OrderNo'];
+        $MemberProductOrderModule = new MemberProductOrderModule();
+        $OrderInfo = $MemberProductOrderModule->GetInfoByWhere(' and OrderNumber = \''.$OrderNumber.'\'');
+        if ($OrderInfo && $OrderInfo['Status']==2) {
+            $Money = $OrderInfo['TotalAmount'];
+            $RedirectUrl = WEB_M_URL . '/orderdetail/'.$OrderNumber.'.html';
+            include template('PayResultSUCCESS');
+        } else {
+            include template('PayResultFAIL');
+        }
+    }
+}
