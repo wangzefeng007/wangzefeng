8c9839059d2c21020711e83958bac5d9776bf8c6
王泽锋 <wangzefeng@longwengui.com>
2017-06-14 17:53:16 +0800

    zf

--- longwengui.com/Controller/M/Debt.php
+++ longwengui.com/Controller/M/Debt.php
@@ -28,6 +28,7 @@ class Debt
         $ID = intval($_GET['ID']);
         //债务信息
         $DebtInfo = $MemberDebtInfoModule->GetInfoByKeyID($ID);
+        $DebtInfo['Overduetime'] = round((time()-$DebtInfo['Overduetime'])/ 86400);
         //债权人信息
         $CreditorsInfo = $MemberCreditorsInfoModule->GetInfoByWhere(" and DebtID = ".$ID,true);
         foreach ($CreditorsInfo as $key=>$value){
