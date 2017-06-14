e02097d1504cd0ddb364140c43810d31e0d6ad3e
web-oop <islvpan@qq.com>
2017-06-14 17:39:11 +0800

    手机端

---
+++ js.longwengui.com/m/debt/DebtDetails.js
@@ -0,0 +1,13 @@
+var pageObj=$.extend({},pageObj,{
+
+    /**
+     * 初始化方法
+     */
+    init:function() {
+        var _this = this;
+
+    }
+})
+$(document).ready(function(){
+    pageObj.init();
+});
