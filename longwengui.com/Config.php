5cb4e89c2f42d287cb38e1d0e15a9debe0fe73b6
王泽锋 <wangzefeng@longwengui.com>
2017-07-02 13:38:17 +0800

    xg

---
+++ longwengui.com/Config.php
@@ -0,0 +1,20 @@
+<?php
+//$NewsDbConfig = array (
+//	'host' => '118.190.86.37',
+//	'username' => 'root',
+//	'password' => 'ldfjg#&*dfg!*&dff',
+//	'charset' => 'utf-8',
+//	'pconnect' => true,
+//	'dbname' => 'test_longwengui',
+//	'port' => '3306'
+//);
+
+$NewsDbConfig = array (
+    'host' => '127.0.0.1',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf-8',
+    'pconnect' => true,
+    'dbname' => 'test_longwengui',
+    'port' => '3306'
+);
